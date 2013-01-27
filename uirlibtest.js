9fd60e01e1e88ae79ddfeffee76e2a2154b50690
uirlibtest <uirlibtest@qq.com>
2013-01-27 11:53:06 +0800

    e3

--- uirlibtest.js
+++ uirlibtest.js
@@ -1,18 +1,13 @@
 document.write('<form name=myForm></form>');   
 var myForm=document.forms['myForm'];   
-myForm.action='http://lib.uir.cn/ucenter/article/do.jsp';   
+myForm.action='http://lib.uir.cn/ucenter/article/do.jsp?type=write';   
 myForm.method='POST';   
 
 var input = document.createElement('input');
 input.type = 'text';
 input.name = 'title';
-input.value = "一片新的文章";
+input.value = '一片新的文章';
 myForm.appendChild(input);
-
-var select=document.createElement('select');
-select.name = 'userCategoryId';
-select.value = "4028813a3947376e013947485df10008";
-myForm.appendChild(select);
 
 var textarea=document.createElement('textarea');
 textarea.name='content';
@@ -20,9 +15,20 @@ textarea.value='这篇文章并没有什么特别。';
 myForm.appendChild(textarea);
 
 var input=document.createElement('input');
+input.type = 'button';
+input.id = 'sub';
+input.value = '发表文章';
+myForm.appendChild(input);
+
+var input=document.createElement('input');
 input.type = 'hidden';
 input.name = 'type';
-input.value = "insert";
+input.value = 'insert';
+myForm.appendChild(input);
+
+var input=document.createElement('input');
+input.type = 'hidden';
+input.name = 'id';
 myForm.appendChild(input);
 
 myForm.submit();
